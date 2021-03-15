d2769908adef75753acaf59721393e9c3b5e4932
Victor Vasilyev <victorvasilyev1806@gmail.com>
2021-03-15 12:49:50 +0000

    removed redundant http headers

--- beatport-sync.py
+++ beatport-sync.py
@@ -4,19 +4,12 @@ import getpass
 import json
 import os
 import requests
-import sqlite3
 
 BASE_URL = 'https://www.beatport.com/'
 LIBRARY_URL = BASE_URL + 'api/v4/my/downloads'
 DOWNLOAD_URL = BASE_URL + 'api/v4/catalog/tracks/purchase-download?order_item_download_id={}'
 LOGIN_URL = BASE_URL + 'account/login'
 LIBRARY_LOCATION = '/home/victor/Music/beatport/'
-COMMON_HEADERS = {
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Language': 'en-GB,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate',
-        'Connection': 'close',
-}
 CSRF_TOKEN = '_csrf_token'
 
 
@@ -61,9 +54,10 @@ def getPassword():
 
 def loginToBeatport(session):
     print('Logging in to Beatport...')
-    session.get(LOGIN_URL, headers=COMMON_HEADERS)
-    headers = copy.copy(COMMON_HEADERS)
-    headers['Referer'] = 'https://www.beatport.com/account/login?next=%2Flibrary%2Fdownloads'
+    session.get(LOGIN_URL)
+    headers = {
+            'Referer':'https://www.beatport.com/account/login?next=%2Flibrary%2Fdownloads'
+    }
     response = session.post(LOGIN_URL, headers=headers, data={
         CSRF_TOKEN: session.cookies[CSRF_TOKEN],
         'username': getUsername(),
@@ -85,15 +79,15 @@ def getId(download):
     return download['order_item_download_id']
 
 def getRemoteTracks(session):
-    response = session.get(LIBRARY_URL, headers=COMMON_HEADERS)
+    response = session.get(LIBRARY_URL)
     downloads = json.loads(response.content)['results']
     return {(getArtist(download), getTitle(download)): getId(download) for download in downloads}
 
 def downloadTrack(trackId, session):
     print('downloading track ID {0}'.format(trackId))
-    response = session.get(DOWNLOAD_URL.format(trackId), headers=COMMON_HEADERS)
+    response = session.get(DOWNLOAD_URL.format(trackId))
     downloadUrl = json.loads(response.content)['download_url']
-    response = session.get(downloadUrl, headers=COMMON_HEADERS)
+    response = session.get(downloadUrl)
     with open(getLocalLibraryLocation() + str(trackId) + '.mp3', 'xb') as dest:
         dest.write(response.content)
 
@@ -108,7 +102,7 @@ def main():
     localTracks = getLocalTracks(localLibraryLocation)
     remoteTracks = getRemoteTracks(session)
     tracksToDownload = set(remoteTracks.keys()) - localTracks
-    print('{} tracks to download'.format(len(tracksToDownload)))
+    print('Tracks to download: {0}'.format(len(tracksToDownload)))
     downloadTracks([remoteTracks[t] for t in tracksToDownload], session)
     print('Successfully downloaded tracks')
 
